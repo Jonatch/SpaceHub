f13bb33d81fa5f79b202fdaf1359f63e6f558c98
Jonatch <115596067+Jonatch@users.noreply.github.com>
2023-11-14 12:57:13 -0500

    Functionality for Demo

--- app/src/main/java/com/example/spacehub/MainActivity.kt
+++ app/src/main/java/com/example/spacehub/MainActivity.kt
@@ -6,15 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.LocalTextInputService
@@ -27,8 +27,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.*
-import org.json.JSONArray
-import org.json.JSONException
 import java.io.IOException
 import java.time.LocalDate
 import java.time.YearMonth
@@ -46,7 +44,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CMEEventList()
+                    NotificationsList()
                 }
             }
         }
@@ -55,10 +53,12 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
     @Composable
-    fun CMEEventList() {
-        var year by remember { mutableStateOf("") }
-        var month by remember { mutableStateOf("") }
+    fun NotificationsList() {
+        var startDate by remember { mutableStateOf("") }
+        var endDate by remember { mutableStateOf("") }
+        var notificationType by remember { mutableStateOf("") }
         var result by remember { mutableStateOf("Loading...") }
+        var notificationsList by remember { mutableStateOf<List<Notification>?>(null) }
 
         val context = LocalContext.current
         val keyboardController = LocalSoftwareKeyboardController.current
@@ -69,23 +69,34 @@ class MainActivity : ComponentActivity() {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            // Year input
+            // Start Date input
             TextField(
-                value = year,
-                onValueChange = { year = it },
-                label = { Text("Enter Year") },
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                value = startDate,
+                onValueChange = { startDate = it },
+                label = { Text("Enter Start Date (yyyy-MM-dd)") },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 8.dp)
             )
 
-            // Month input
+            // End Date input
             TextField(
-                value = month,
-                onValueChange = { month = it },
-                label = { Text("Enter Month") },
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                value = endDate,
+                onValueChange = { endDate = it },
+                label = { Text("Enter End Date (yyyy-MM-dd)") },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+            )
+
+            // Notification Type input
+            TextField(
+                value = notificationType,
+                onValueChange = { notificationType = it },
+                label = { Text("Enter Notification Type") },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 8.dp)
@@ -93,19 +104,18 @@ class MainActivity : ComponentActivity() {
 
             Spacer(modifier = Modifier.height(16.dp))
 
+            // Button click handling
             Button(
                 onClick = {
                     val apiKey = "PzCNkrm4dtZGYZS4EyBajyzbFROa4hbM109iqME5" // Replace with your actual API key
-                    val startDate = "${year}-${month}-01"
-                    val endDate = "${year}-${month}-${YearMonth.of(year.toInt(), month.toInt()).lengthOfMonth()}"
 
                     coroutineScope.launch {
                         try {
-                            val cmeResultList = runCMEFetch(startDate, endDate, apiKey)
-                            result = if (cmeResultList != null) {
-                                processAndFormatCMEList(cmeResultList)
+                            notificationsList = runNotificationsFetch(startDate, endDate, notificationType, apiKey)
+                            result = if (notificationsList != null) {
+                                "Notifications fetched successfully"
                             } else {
-                                "Error fetching CME results"
+                                "Error fetching notifications"
                             }
                         } catch (e: Exception) {
                             result = "Error: ${e.message ?: "Unknown error"}"
@@ -116,49 +126,76 @@ class MainActivity : ComponentActivity() {
             ) {
                 Icon(imageVector = Icons.Default.Search, contentDescription = null)
                 Spacer(modifier = Modifier.width(4.dp))
-                Text("Fetch CME Data")
+                Text("Fetch Notifications Data")
             }
 
             Spacer(modifier = Modifier.height(16.dp))
 
+            // Display notifications in a scrollable column
+            notificationsList?.let { notifications ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    val uniqueMessageIds = notifications.map { it.messageID }.distinct()
+                    for (messageId in uniqueMessageIds) {
+                        val filteredNotifications = notifications.filter { it.messageID == messageId }
+                        NotificationCardGroup(notifications = filteredNotifications)
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                }
+            }
+
+            // Display result message
             Text(result)
         }
     }
 
-    data class CMEResult(
-        val activityID: String,
-        val catalog: String,
-        val startTime: String,
-        val sourceLocation: String?,
-        val activeRegionNum: Int?,
-        val link: String,
-        val note: String,
-        val instruments: List<Instrument>,
-        val cmeAnalyses: List<CMEAnalysis>,
-        val linkedEvents: List<Any>?
+    @Composable
+    fun NotificationCardGroup(notifications: List<Notification>) {
+        Column {
+            for (notification in notifications) {
+                NotificationCard(notification = notification)
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
+
+    @Composable
+    fun NotificationCard(notification: Notification) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text("Message ID: ${notification.messageID}")
+                Text("Message Type: ${notification.messageType}")
+                Text("Issue Time: ${notification.messageIssueTime}")
+                Text("Message Body: ${notification.messageBody}")
+                Text("Message URL: ${notification.messageURL}")
+            }
+        }
+    }
+
+    data class Notification(
+        val messageID: String,
+        val messageBody: String,
+        val messageType: String,
+        val messageIssueTime: String,
+        val messageId: Int,
+        val messageURL: String
     )
 
-    data class Instrument(
-        val displayName: String
-    )
-
-    data class CMEAnalysis(
-        val time21_5: String,
-        val latitude: Double,
-        val longitude: Double?,
-        val halfAngle: Double,
-        val speed: Double,
-        val type: String,
-        val isMostAccurate: Boolean,
-        val note: String,
-        val levelOfData: Int,
-        val link: String,
-        val enlilList: List<Any>?
-    )
-
-    private suspend fun runCMEFetch(startDate: String, endDate: String, apiKey: String): List<CMEResult>? =
+    private suspend fun runNotificationsFetch(startDate: String, endDate: String, type: String, apiKey: String): List<Notification>? =
         withContext(Dispatchers.IO) {
-            val url = "https://api.nasa.gov/DONKI/CME?startDate=$startDate&endDate=$endDate&api_key=$apiKey"
+            val url = "https://api.nasa.gov/DONKI/notifications?" +
+                    "startDate=$startDate&endDate=$endDate&type=$type&api_key=$apiKey"
             val request = Request.Builder().url(url).build()
 
             client.newCall(request).execute().use { response ->
@@ -171,49 +208,8 @@ class MainActivity : ComponentActivity() {
                     return@withContext null
                 } else {
                     val jsonString = response.body?.string()
-                    return@withContext Gson().fromJson(jsonString, Array<CMEResult>::class.java).toList()
+                    return@withContext Gson().fromJson(jsonString, Array<Notification>::class.java).toList()
                 }
             }
         }
-
-    private fun processAndFormatCMEList(cmeResultList: List<CMEResult>?): String {
-        val resultStringBuilder = StringBuilder()
-
-        if (cmeResultList != null) {
-            for (cmeResult in cmeResultList) {
-                val processedData = processCMEData(cmeResult.cmeAnalyses)
-                resultStringBuilder.append("Event Details:\n")
-                resultStringBuilder.append("Time: ${cmeResult.startTime}\n")
-                resultStringBuilder.append("Location: ${cmeResult.sourceLocation ?: "N/A"}\n")
-                resultStringBuilder.append("Note: ${cmeResult.note}\n\n")
-                resultStringBuilder.append("CME Analysis Data:\n")
-                resultStringBuilder.append(processedData)
-                resultStringBuilder.append("\n\n")
-            }
-        }
-
-        return resultStringBuilder.toString()
-    }
-
-    private fun processCMEData(cmeAnalyses: List<CMEAnalysis>): String {
-        val processedDataList = mutableListOf<String>()
-
-        for (cmeAnalysis in cmeAnalyses) {
-            val whenValue = cmeAnalysis.time21_5
-            val latitude = cmeAnalysis.latitude
-            val longitude = cmeAnalysis.longitude ?: Double.NaN
-            val note = cmeAnalysis.note
-
-            val entry = "When: $whenValue, Where: Latitude $latitude, Longitude ${
-                if (longitude.isNaN()) "N/A" else longitude.toString()
-            }, Note: $note"
-
-            processedDataList.add(entry)
-        }
-
-        return processedDataList.joinToString("\n")
-    }
 }
-
-
-
